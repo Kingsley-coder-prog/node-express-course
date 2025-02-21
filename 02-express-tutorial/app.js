869d0bbe26b6630cc6d21f1a288757734a9c0995
Kingsley <kifeanyi216@gmail.com>
2025-02-21 15:42:22 +0100

    Express basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,53 +1,26 @@
-console.log("Express Tutorial");
+const express = require("express");
+const app = express();
 
-const http = require("http");
-const { readFileSync } = require("fs");
-
-// get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  console.log(url);
-  // home page
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  }
-  // about page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About Page</h1>");
-    res.end();
-  }
-  // styles
-  else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyles);
-    res.end();
-  }
-  // image/logo
-  else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-    res.end();
-  }
-  // logic
-  else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-    res.end();
-  }
-  // 404 page
-  else {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Page not found!</h1>");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.status(200).send("Home Page");
 });
 
-server.listen(5000);
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>Resource not found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
