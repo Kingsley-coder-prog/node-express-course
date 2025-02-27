b0cd4ac9a0aa193867025f76e2c85e3c5c9039bb
Kingsley <kifeanyi216@gmail.com>
2025-02-27 08:26:20 +0100

    Additional Middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const logger = require("./logger");
 const authorize = require("./authorize");
-
 // req => middleware => res
-app.use([logger, authorize]);
+
+// 1. use vs route
+// 2. options - our own / express / third party middleware
+
+// app.use([logger, authorize]);
+// app.use(express.static("./public"));
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Home Page");
