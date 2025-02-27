b7f3ac4052af95c011b3629ca4cc34d76e50c39f
Kingsley <kifeanyi216@gmail.com>
2025-02-27 08:39:49 +0100

    Get Method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,31 +1,9 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const logger = require("./logger");
-const authorize = require("./authorize");
-// req => middleware => res
+let { people } = require("./data");
 
-// 1. use vs route
-// 2. options - our own / express / third party middleware
-
-// app.use([logger, authorize]);
-// app.use(express.static("./public"));
-app.use(morgan("tiny"));
-
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page");
-});
-app.get("/api/products", (req, res) => {
-  res.send("Products");
-});
-
-app.get("/api/items", (req, res) => {
-  console.log(req.user);
-  res.send("Items");
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
 
 app.listen(5000, () => {
