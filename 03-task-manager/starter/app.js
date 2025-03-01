8b4c645622f665bd63fe8569881ff2afdc325e32
Kingsley <kifeanyi216@gmail.com>
2025-03-01 17:27:06 +0100

    Express Server

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+// const router = express.Router();
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager Page");
+});
+
+// app.get('/api/v1/tasks) - get all the tasks
+// app.post('/api/v1/tasks) - create new tasks
+// app.get('/api/v1/tasks/:id) - get a single task
+// app.patch('/api/v1/tasks/:id) - update task
+// app.delete('/api/v1/tasks/:id) - delete task
+
+const port = 3000;
+
+app.listen(3000, console.log(`Server is listening on port ${port}...`));
