ec86c893a98d09b4676abe54e0aa6a42372c2014
Kingsley <kifeanyi216@gmail.com>
2025-03-11 14:21:43 +0100

    Setup Project 2

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,9 +1,13 @@
+// const { getAllProductsStatic } = require("./controllers/products");
 require("dotenv").config();
 
-// asyn errors
+// async errors
 
 const express = require("express");
 const app = express();
+
+const connectDB = require("./db/connect");
+const productRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -18,6 +22,7 @@ app.get("/", (req, res) => {
 });
 
 // products route
+app.use("/api/v1/products", productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -27,6 +32,7 @@ const port = process.envPORT || 3000;
 const start = async () => {
   try {
     // connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
