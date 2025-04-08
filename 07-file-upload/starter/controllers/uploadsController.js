19783f70b952f180e54f65c77ced807a52fd1db5
Kingsley <kifeanyi216@gmail.com>
2025-04-08 09:09:36 +0100

    Complete File Uploads

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,9 +1,19 @@
-const Product = require("../models/Product");
+const path = require("path");
 const { StatusCodes } = require("http-status-codes");
 
 const uploadProductImage = async (req, res) => {
-  console.log(req);
-  res.send("upload product image");
+  console.log(req.files);
+  let productImage = req.files.image;
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = {
