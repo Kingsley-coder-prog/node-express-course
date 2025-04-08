33fe7eecc49c598eb6a731cd366ea785e285b0e0
Kingsley <kifeanyi216@gmail.com>
2025-04-08 11:06:55 +0100

    Remove Temp Files

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
 const uploadProductImageLocal = async (req, res) => {
   // console.log(req.files);
@@ -40,6 +41,8 @@ const uploadProductImage = async (req, res) => {
       folder: "file-upload",
     }
   );
+  fs.unlinkSync(req.files.Image.tempFilePath);
+  console.log(result);
   return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
