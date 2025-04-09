9561314d7a390804e0b25fd3a19225648449182f
Kingsley <kifeanyi216@gmail.com>
2025-04-09 10:45:17 +0100

    Complete Payment

--- 09-stripe-payment/starter/controllers/stripeController.js
+++ 09-stripe-payment/starter/controllers/stripeController.js
@@ -1,6 +1,18 @@
-const stripe = async (req, res) => {
-  console.log(req.body);
-  res.send("stripe route");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const stripeController = async (req, res) => {
+  const { purchase, total_amount, shipping_fee } = req.body;
+
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee;
+  };
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: "usd",
+  });
+  console.log(paymentIntent);
+  res.json({ clientSecret: paymentIntent.client_secret });
 };
 
-module.exports = stripe;
+module.exports = stripeController;
