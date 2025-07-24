6b24c05dd57f44066c0cb208039930c346527e40
Kingsley <kifeanyi216@gmail.com>
2025-07-24 14:13:06 +0100

    Connect to DB

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,12 +1,16 @@
+require("dotenv").config();
+
 // express
 
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 5000;
+const connectDB = require("./db/connect");
 
+const port = process.env.PORT || 5000;
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, console.log(`Server is running on port ${port}`));
   } catch (error) {
     console.log("Error connecting to the database:", error);
