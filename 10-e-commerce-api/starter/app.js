01ad33555186463d7b49afcbf57b5354aecfd7db
Kingsley <kifeanyi216@gmail.com>
2025-07-24 13:48:08 +0100

    Basic Express Server

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,16 @@
-console.log('E-Commerce API');
+// express
+
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is running on port ${port}`));
+  } catch (error) {
+    console.log("Error connecting to the database:", error);
+  }
+};
+
+start();
