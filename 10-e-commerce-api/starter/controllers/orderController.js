67e1e910832419561c9c5fabd52f5090880704c6
Kingsley <kifeanyi216@gmail.com>
2025-08-08 20:57:24 +0100

    Create Order - Complete

--- 10-e-commerce-api/starter/controllers/orderController.js
+++ 10-e-commerce-api/starter/controllers/orderController.js
@@ -4,6 +4,11 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const { checkPermissions } = require("../utils");
 
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = "someRandomValue";
+  return { client_secret, amount };
+};
+
 const createOrder = async (req, res) => {
   const { items: cartItems, tax, shippingFee } = req.body;
 
@@ -41,10 +46,25 @@ const createOrder = async (req, res) => {
     // calculate subtotal
     subtotal += item.amount * price;
   }
-  console.log(orderItems);
-  console.log(subtotal);
+  // calculate total
+  const total = tax + shippingFee + subtotal;
+  // get client secret
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: "usd",
+  });
 
-  res.send("create order");
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+
+  res.status(StatusCodes.OK).json({ order, clientSecret: order.clientSecret });
 };
 const getAllOrders = async (req, res) => {
   res.send("get all orders");
